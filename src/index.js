d99b2bf179419a394efa2b8f43f7eb7722b72f40
SonicReal <alisher.nuritdinov@payme.uz>
2019-09-23 11:03:42 +0500

    update: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const map = {};
+    for (const cnf of bracketsConfig) {
+        map[cnf[0]] = cnf[0] !== cnf[1] ? cnf[1] : 'same';
+    }
+    if (str.length === 0) {
+        return true;
+    }
+    if (str.length % 2 === 1) {
+        return false;
+    }
+    const stack = [];
+    for (let i = 0; i < str.length; i++) {
+        const current = str[i];
+        if (map[current]) {
+            if (map[current] !== 'same' || stack[stack.length - 1] !== current) {
+                stack.push(current);
+            } else {
+                stack.pop();
+            }
+            continue;
+        }
+        if (map[stack[stack.length - 1]] === current) {
+            stack.pop();
+        } else {
+            return false;
+        }
+    }
+    return stack.length === 0;
 }
